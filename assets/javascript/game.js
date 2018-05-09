bc10554c32d85c30ad8c8ff55212d38548833119
robidobi33 <robidobi33@gmail.com>
2018-05-09 12:37:08 -0600

    update_02

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+var homer, bart, maggie, ned, health = 0, her, enemies = [], characters = [];
+
+characters = [
+
+homer = {
+    name: "Homer",
+    health: 300,
+    attack: Math.floor(Math.random() * 10)
+},
+
+bart = {
+    name: "Bart",
+    health: 175,
+    attack: Math.floor(Math.random() * 10) + 10
+},
+
+maggie = {
+    name: "maggie",
+    health: 90,
+    attack: Math.floor(Math.random() * 20) + 10
+},
+ ned = {
+     name: "Flanders",
+     health: 230,
+     attack: Math.floor(Math.random() * 10) + 5
+ }
+
+]
+
+ console.log(maggie.attack);
+
+ /*switch(){
+    case :
+    break;
+
+ }
+
+function startAttack(hero, enemies){
+    alert("hello" + hero);
+}*/
